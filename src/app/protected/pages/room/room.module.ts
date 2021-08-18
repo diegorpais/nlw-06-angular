0afaedc30fab5170813be026b2325a86c007745b
Diego Raphael Borges Pais <draphae5@ford.com>
2021-08-18 14:19:42 -0300

    feat(room, loading): componentizando loading das paginas

--- src/app/protected/pages/room/room.module.ts
+++ src/app/protected/pages/room/room.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { LoadingModule } from 'src/app/public/components/loading/loading.module';
 import { RoomCodeModule } from 'src/app/protected/components/room-code/room-code.module';
 
 import { RoomComponent } from './room.component';
@@ -20,6 +21,7 @@ const routes: Routes = [
     CommonModule,
     RoomCodeModule,
     ReactiveFormsModule,
+    LoadingModule,
     RouterModule.forChild(routes)
   ]
 })
